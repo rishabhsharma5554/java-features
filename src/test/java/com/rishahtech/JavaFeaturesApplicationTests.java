7f1b4ca6c6aee3fad3b15553bbbe924f1315126f
Rishabh Sharma <rishabhsharma.5554@gmail.com>
2025-08-11 09:47:46 +0530

    Initial commit

---
+++ src/test/java/com/rishahtech/JavaFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishahtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
