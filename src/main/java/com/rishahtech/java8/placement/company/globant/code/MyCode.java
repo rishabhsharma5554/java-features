9a1a1dda38ceb742bc6c0727ea93f5f3f8e24675
Rishabh Sharma <89383192+rishabhsharma5554@users.noreply.github.com>
2026-04-13 09:14:49 +0530

    Add files via upload

---
+++ src/main/java/com/rishahtech/java8/placement/company/globant/code/MyCode.java
@@ -0,0 +1,20 @@
+//Write a Java program to print all characters from a string that occur an even number of times along with their frequency, using Java Streams.
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+
+
+public class MyCode {
+  public static void main(String args[]) {
+    String str = "Prathammmm";
+    str.chars()
+    .mapToObj(c-> (char) c)
+    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+    .entrySet()
+    .stream()
+    .filter(e -> e.getValue() % 2 == 0)
+    .forEach(e -> System.out.println(e.getKey() +" -> " +e.getValue()));
+  }
+}
