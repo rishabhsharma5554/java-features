142748f270b7d59a94f33f8697732a1f091d8f57
Rishabh Sharma <89383192+rishabhsharma5554@users.noreply.github.com>
2026-04-13 09:15:40 +0530

    Refactor MyCode.java with package declaration and comments

--- src/main/java/com/rishahtech/java8/placement/company/globant/code/MyCode.java
+++ src/main/java/com/rishahtech/java8/placement/company/globant/code/MyCode.java
@@ -1,10 +1,11 @@
-//Write a Java program to print all characters from a string that occur an even number of times along with their frequency, using Java Streams.
+package com.rishahtech.java8.placement.company.globant.code;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.function.Function;
 
+//Write a Java program to print all characters from a string that occur an even number of times along with their frequency, using Java Streams.
 
 public class MyCode {
   public static void main(String args[]) {
